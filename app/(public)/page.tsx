44991e4af9f3bcddf7d984c5ecca587a5ea4e7e7
leadond <leadond@gmail.com>
2025-08-23 07:57:26 -0500

    Remove conflicting (public) route group to fix Vercel build error

--- app/(public)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export const dynamic = 'force-dynamic'
-
-export default function PublicRootRedirect() {
-  redirect('/home') // or any clean path you want to expose
-  return null
-}
